9884ed63c00860d4df82f086e948e144b3925510
Luxiah <141586861+Luxiah@users.noreply.github.com>
2025-03-06 19:55:06 -0500

    messing around with buoyancy

--- scripts/entities/floating_box.gd
+++ scripts/entities/floating_box.gd
@@ -1,16 +1,4 @@
-class_name FloatingBox
-extends CharacterBody3D
+extends BuoyancyBody3D
 
-var buoyancy : float = 10
-var surface : float
 
-static var instance = self
-
-static func get_wave_height(height: float):
-	instance.surface = height
-	
-
-func _physics_process(delta):
-	if position.y < surface:
-		velocity.y += buoyancy
 	
